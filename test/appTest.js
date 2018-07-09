b6312b7bca47721277f8ad52c8bf7d1474943459
ZaliyhaMahmood <mzaliyha@yahoo.com>
2018-07-09 05:02:18 +0300

    First commit

---
+++ test/appTest.js
@@ -0,0 +1,40 @@
+var expect = require('chai').expect
+var request = require('request')
+var app = require('../app').app;
+
+
+describe('Server API Test', () => {
+  describe('RGB to Hex conversion', () => {
+    var url = 'http://localhost:3000/rgbToHex?red=255&green=255&blue=255'
+    it('returns status 200', (done) => {
+      request(url, (error, response, body) => {
+        expect(response.statusCode).to.equal(200)
+        done();
+      })
+    })
+
+    it('returns the color in hex', (done) => {
+      request(url, (error, response, body) => {
+        expect(body).to.equal('ffffff')
+        done();
+      })
+    })
+  })
+
+  describe('Hex to RGB conversion', () => {
+    var url = 'http://localhost:3000/hexToRgb?hex=00ff00'
+    it('returns status 200', (done) => {
+      request(url, (error, response, body) => {
+        expect(response.statusCode).to.equal(200);
+        done();
+      })
+    })
+
+    it('returns the color in RGB', (done) => {
+      request(url, (error, response, body) => {
+        expect(body).to.equal('[0,255,0]')
+        done();
+      })
+    })
+  })
+})
